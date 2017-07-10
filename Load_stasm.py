ceaa3764113807308c0db6f55b0945badfb73316
Muhammad Zeshan <zeshan.mushtaq@ucp.edu.pk>
2017-07-09 23:24:09 -0700

    Update Load_stasm.py

--- Load_stasm.py
+++ Load_stasm.py
@@ -3,12 +3,33 @@ import pandas as pd
 import cv2
 import stasm
 
+
+
+path = os.path.join(stasm.DATADIR, 'testface.jpg')
+
+img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+if img is None:
+    print("Cannot load", path)
+    raise SystemExit
+
+landmarks = stasm.search_single(img)
+
+if len(landmarks) == 0:
+    print("No face found in", path)
+else:
+    landmarks = stasm.force_points_into_image(landmarks, img)
+    for point in landmarks:
+        img[round(point[1])][round(point[0])] = 255
+
+cv2.imshow("stasm minimal", img)
 #Read images from .csv and apply stasm to detect landmarks
 #Compute RMSE and show results
 
+
 w= 96 #image width
 h= 96 #image height
-number_of_keypoints= 15
+number_of_keypoints=landmarks
 
 def LoadTrainData(filename, flDropNaNs= False):
 	df = pd.read_csv(filename, header=0) #(7049L, 31L)
